40f2c98910c7a6559b226d2612ca8f179837a04d
Will Storey <will@willstorey.ca>
2020-03-03 12:56:39 -0500

    Implemented Hot Jar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,14 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+document.addEventListener('turbolinks;load', function(event) {
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1714099,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+});
