31a4b0869e732d14046f6fa6e1550b76850bf07d
Will Storey <will@willstorey.ca>
2020-03-03 11:13:22 -0500

    Implemented Google Analytics

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'pg', '~> 0.21'
 
 gem 'devise'
 
+gem 'rack-tracker'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,10 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.middleware.use(Rack::Tracker) do
+    handler :google_analytics, { tracker: 'UA-159599654-1' }
+  end
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
